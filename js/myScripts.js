81381d841937f858e9f139b90b737fd6db2f2194
Natasha <47771734+NR480@users.noreply.github.com>
2019-10-08 00:04:25 -0700

    Updated all files
    Updated HTML, CSS and JS files

--- js/myScripts.js
+++ js/myScripts.js
@@ -1,22 +1,25 @@
 var dogRepository = (function() {
     var newdogRepository = [
-        { name: "Baxter", height: 16, color: "Brown", breed: "Basenji" },
-        { name: "Pokemon", height: 26, color: "Black", breed: "Doberman" },
-        { name: "Lucy", height: 10, color: "Grey", breed: "Maltese" }
+        { breed: "Basenji", name: "Baxter", age: "6 months" },
+        { breed: "Doberman", name: "Pokemon", age: "1 yr" },
+        { breed: "Maltese", name: "Lucy", age: "4 yrs" },
+        { breed: "German Shepherd", name: "Cesar", age: "5 yrs" },
+        { breed: "Spaniel", name: "Candy", age: "3 yrs" },
+        { breed: "Akita", name: "Lucky", age: "11 months" }
     ];
     return {
         getAll: function() {
             return newdogRepository;
         },
         add: function(item) {
-            newdogRepository.push({ name: item.name, height: item.height, color: item.color, breed: item.breed });
+            newdogRepository.push({ name: item.name, age: item.age, breed: item.breed });
         },
         addListItem: function(dog) {
             var listItem = document.createElement('li');
             var dogButton = document.createElement('button');
 
             // create the button
-            dogButton.innerText = dog.name;
+            dogButton.innerText = dog.breed;
             dogButton.classList.add('dog-button');
             dogButton.addEventListener('click', function(event) {
                 this.showDetails(dog);
